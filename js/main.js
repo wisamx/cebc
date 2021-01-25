6936d4bb7b4f6b6314ad315b5b70de34f0b00f3a
Wisamx <75364006+wisammindfield@users.noreply.github.com>
2021-01-25 09:56:27 +0200

    c1

--- js/main.js
+++ js/main.js
@@ -326,5 +326,54 @@ setInterval(function() { makeTimer(); }, 1000);
 $("#website-header").load("header.html");
 $("#website-footer").load("footer.html");
 
+
+	var data;
+	$.ajax({
+	  type: "GET",  
+	  url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-01S4T-HywuIQMoOYJ498vmn3SyaHQFZ-0YfLBR8QhR7_k7xX6BVKCadOsa_-Lz9oFALvm3cSKprf/pub?output=csv",
+	  dataType: "text",
+	  success: function(response)
+	  {
+		data = $.csv.toArrays(response);
+		generateHtmlTable(data);
+	  }
+	});
+
+	function generateHtmlTable(data) {
+		
+	  var html = '<table  class="table table-condensed table-hover table-striped">';
+ 
+      if(typeof(data[0]) === 'undefined') {
+        return null;
+      } else {
+		$.each(data, function( index, row ) {
+		  //bind header
+		  if(index == 0) {
+			html += '<thead>';
+			html += '<tr>';
+			$.each(row, function( index, colData ) {
+				html += '<th>';
+				html += colData;
+				html += '</th>';
+			});
+			html += '</tr>';
+			html += '</thead>';
+			html += '<tbody>';
+		  } else {
+			html += '<tr>';
+			$.each(row, function( index, colData ) {
+				html += '<td>';
+				html += colData;
+				html += '</td>';
+			});
+			html += '</tr>';
+		  }
+		});
+		html += '</tbody>';
+		html += '</table>';
+		$('#csv-display').append(html);
+	  }
+	}
+	
 })(jQuery);
 
